2d210cc60b3fa8adccafc8b483ffff4979ce346c
Jaume <jaume1279@gmail.com>
2019-09-09 01:50:06 +0200

    Initial commit

---
+++ Notas/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Notas
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            string nombre;
+            double nota1, nota2, nota3;
+            double promedio;
+
+            Console.Write("Nombre del alumno:");
+            nombre = (Console.ReadLine());
+
+            Console.Write("Nota1:");
+            nota1=double.Parse(Console.ReadLine());
+
+            Console.Write("Nota2:");
+            nota2 = double.Parse(Console.ReadLine());
+
+            Console.Write("Nota3:");
+            nota3 = double.Parse(Console.ReadLine());
+
+            promedio = (nota1 + nota2 + nota3) / 3;
+            Console.Write(nombre + " ha obtenido un promedio de: " + promedio.ToString("0.00"));
+
+            Console.ReadLine();
+
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,2 @@
+# Cs_Notas
+ Projecte C#
